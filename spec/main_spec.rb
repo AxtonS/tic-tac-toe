f0bd0957102691546b2e72e5772c45a8f941f4c7
Axton Smith <axtonsmith@gmail.com>
2023-10-21 15:15:34 -0400

    Move gameplay logic into script.rb

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,47 +1,13 @@
 # frozen_strin_literal: true
 
-require '../lib/main.rb'
+require_relative '../lib/main.rb'
 
 describe GameState do
-  subject(:game) { described_class.new }
-
-  describe '#victory_condition' do
-
-    it 'return true when top row is same sign' do
-      3.times { |i| game.place_sign(i, 'X')}
-      expect(game.victory_condition('X')).to eq(true)
-    end
-
-    it 'return true when left column is same sign' do
-      i = 0
-      3.times do
-        game.place_sign(i, 'O')
-        i += 3
-      end
-      expect(game.victory_condition('O')).to eq(true)
-    end
-
-    it 'return true when diagonal top left to bottom right is same sign' do
-      i = 0
-      3.times do
-        game.place_sign(i, 'A')
-        i += 4
-      end
-      expect(game.victory_condition('A')).to eq(true)
-    end
-
-    it 'return nil when sign has not won' do
-      expect(game.victory_condition('O')).to be_nil
-    end
-  end
-
-  describe '#place_sign' do
-
-    it 'places the correct sign in correct space' do
-      space = 0
-      sign = 't'
-      game.place_sign(space, sign)
-      expect(game.board[space]).to eq(sign)
+  describe "#place_sign" do
+    subject(:game) { described_class.new }
+    it "places string into given space on board" do
+      game.place_sign(0, 'x')
+      expect(game.board[0]).to eq('x')
     end
   end
 end
