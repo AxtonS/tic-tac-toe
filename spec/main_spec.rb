4b3ce597a86ff26fd605f6609e488e13ac59a7b6
Axton Smith <axtonsmith@gmail.com>
2023-10-21 15:40:58 -0400

    Write tests for #victory_condition

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -15,4 +15,36 @@ describe GameState do
       expect(game.board[9]). to be_nil
     end
   end
+
+  describe "#victory_condition" do
+    subject(:game) { described_class.new }
+    it "returns true if top row is same sign" do
+      game.instance_variable_set(:@board, ['x', 'x', 'x', ' ', ' ', ' ', ' ', ' ', ' '])
+      expect(game.victory_condition('x')).to eql true
+    end
+
+    it "returns true if middle row is same sign" do
+      game.instance_variable_set(:@board, [' ', ' ', ' ', 'a', 'a', 'a', ' ', ' ', ' '])
+      expect(game.victory_condition('a')).to eql true
+    end
+
+    it "returns true if bottom row is same sign" do
+      game.instance_variable_set(:@board, [' ', ' ', ' ', ' ', ' ', ' ', 'z', 'z', 'z'])
+      expect(game.victory_condition('z')).to eql true
+    end
+
+    it "returns true if forward diagonal is same sign" do
+      game.instance_variable_set(:@board, [' ', ' ', 'y', ' ', 'y', ' ', 'y', ' ', ' '])
+      expect(game.victory_condition('y')).to eql true
+    end
+
+    it "returns true if backward diagonal is same sign" do
+      game.instance_variable_set(:@board, ['G', ' ', ' ', ' ', 'G', ' ', ' ', ' ', 'G'])
+      expect(game.victory_condition('G')).to eql true
+    end
+
+    it "returns nil if no winner" do
+      expect(game.victory_condition('x')).to be_nil
+    end
+  end
 end
